51f18151a8e98fad69e3cd1cc0c248193cc5055a
Chris Cheetham <chris@cheetham.com>
2020-06-29 10:27:38 -0400

    Add signing secrets variables [#12]

--- sign/SignPackages.ps1
+++ sign/SignPackages.ps1
@@ -2,12 +2,29 @@
 
 $baseDir = "$PSScriptRoot/.."
 $toolDir = "$baseDir/tools"
+$artifactStagingDirectory = $Env:ArtifactStagingDirectory
+
+$name = "Steeltoe"
+$description = "Steeltoe"
+$descriptionUrl = "https://github.com/SteeltoeOSS"
 $appSettings = "$baseDir/sign/appsettings.json"
-$fileList = "$baseDir/sign/filelist.txt"
+$signClientUser = $Env:SignClientUser
+$signClientSecret = $Env:SignClientSecret
 
-"installing signclient"
+"installing SignClient"
 New-Item -ItemType Directory -Force -Path "$toolDir"
-dotnet tool install --tool-path "$toolDir" signclient
+dotnet tool install --tool-path "$toolDir" SignClient
 
-"signing"
-& $toolDir/SignClient Sign --input "**/*.nupkg" --config "$appSettings" --filelist "$fileList" --user $Env:SignClientUser --secret $Env:SignClientSecret --name "Steeltoe" --description "Steeltoe" --descriptionUrl "https://github.com/SteeltoeOSS"
+"looking for artifacts"
+$artifacts = Get-ChildItem $artifactStagingDirectory/Steeltoe*.*nupkg -recurse | Select-Object -ExpandProperty FullName
+if ($artifacts) {
+    foreach ($artifact in $artifacts)
+    {
+        "signing $artifact"
+        & $toolDir/SignClient Sign --input $artifact --config $appSettings --user $signClientUser --secret $signClientSecret --name $name --description $description --descriptionUrl $descriptionUrl
+    }
+}
+else
+{
+    "no artifacts found"
+}
